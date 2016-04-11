c7e5922b7f3a73070471171f6627fc72b6fe62c1
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 13:04:56 -0700

    Created count_words function, wordcount.py can now be run from command line with text file as argument

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,28 @@
-# each word is the key, value is the count of that word
-word_count = {}
-# open a text file
-with open('twain.txt') as in_file:
-    for line in in_file:
-        words = line.split()
-        for word in words:
-            # make sure the word isn't already in the word_count dict
-            if word not in word_count:
-                word_count[word] = 1
-            # word is already in the word_count dict, so increment value by one
-            else:
-                word_count[word] += 1
+from sys import argv
 
-#print word_count
-for word, count in word_count.items():
-    print word, count
+file_name = argv[1]
+def count_words(file_name):
+    """Take name of text file, returns None.
+        Counts occurence of each word.
+    """
+    # each word is the key, value is the count of that word
+    word_count = {}
+    # open a text file
+    with open(file_name) as in_file:
+        for line in in_file:
+            words = line.split()
+            for word in words:
+                word = word.lower()
+                word = word.replace('.', '').replace(',', '').replace('?', '')
+                # make sure the word isn't already in the word_count dict
+                if word not in word_count:
+                    word_count[word] = 1
+                # word is already in the word_count dict, so increment value by one
+                else:
+                    word_count[word] += 1
+
+    #print word_count
+    for word, count in word_count.items():
+        print word, count
+
+count_words(file_name)
